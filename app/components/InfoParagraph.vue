255499b812a59aa1c27fa500a5efa7f5ab5697b2
Boston Impressions <bostonimpressions@gmail.com>
2025-08-27 10:52:29 -0400

    start comps and main css defs

--- app/components/InfoParagraph.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <section class="text-black max-w-3xl mx-auto px-4">
-    <ContentDoc path="/home/intro" />
-  </section>
-</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,21 +1,17 @@
 <script setup lang="ts">
-const { data: intro } = await useAsyncData(() =>
-  queryCollection('content')
-    .path('/home/intro')
-    .first()
-)
+  const { data: intro } = await useAsyncData(() =>
+    queryCollection('content').path('/home/intro').first()
+  )
 </script>
 
 <template>
-  <div>
-    nexum
-  </div>
+  <div>nexum</div>
 </template>
 
 <style lang="scss" scoped>
-
-
-@media (min-width: 768px) {
-
-}
+  @media (min-width: 768px) {
+    .test {
+      color: red;
+    }
+  }
 </style>
