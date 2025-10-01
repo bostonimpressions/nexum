0c88c311b84c1a4855caa5fd6373faab6308d137
Boston Impressions <bostonimpressions@gmail.com>
2025-10-01 08:04:51 -0400

    first defense converted

--- app/pages/services/first-defense.vue
+++ app/pages/services/first-defense.vue
@@ -6,6 +6,7 @@
     'overview',
     '/services/first-defense/overview'
   )
+  //overview two content
   const overviewTwo = await useContentSection(
     'overviewTwo',
     '/services/first-defense/overview-two'
@@ -13,62 +14,20 @@
   const overviewTwoImage = computed(
     () => overviewTwo.value?.meta?.imgs?.[0] || {}
   )
-
-  interface iconWithText {
+  //overview three content
+  const overviewThree = await useContentSection(
+    'overviewThree',
+    '/services/first-defense/overview-three'
+  )
+  interface IconWithText {
     imgSrc: string
     imgAlt: string
     title: string
     text: string
   }
-
-  const serviceFeatures: iconWithText[] = [
-    {
-      imgSrc: '/icons/icon-fd-speed.png',
-      imgAlt: 'Speed',
-      title: '1. Speed',
-      text: `Rapid collection and processing get alerts to our analysts
-      usually within 60 seconds. Notification of important events to
-      customers occurs within minutes and emergency changes are
-      processed promptly which means faster time to resolution and
-      less risk of a breach.`,
-    },
-    {
-      imgSrc: '/icons/icon-fd-data.png',
-      imgAlt: 'Data',
-      title: '2. Threat Intelligence',
-      text: `Data from over 100 sources of threat intelligence are ingested
-      in FD solution to identify malicious activity and proactively
-      prevent harm to your organization. Access to consolidated threat
-      intelligence feeds is available to all FD customer.`,
-    },
-    {
-      imgSrc: '/icons/icon-fd-capabilities.png',
-      imgAlt: 'Data',
-      title: '3. Security Monitoring and Management',
-      text: `Advanced detection and automation capabilities are provided for your systems
-      (whether you have your own SIEM or leverage Nexum’s). The solution is
-      delivered with accountability in mind and customers can elect to retain some
-      or all of the control and activity of the managed platform.`,
-    },
-    {
-      imgSrc: '/icons/icon-fd-procedure.png',
-      imgAlt: 'Data',
-      title: '4. Flexibility and Transparency',
-      text: `Procedures specific to your organization are customized and transparent access
-      is provided to all workflow notes, even for false positives and non-critical events.
-      Alerts are clear, concise, with a simple outline of cause and a straightforward
-      direction to resolution.`,
-    },
-    {
-      imgSrc: '/icons/icon-fd-engineers.png',
-      imgAlt: 'Data',
-      title: '5. People',
-      text: `Analysts at our SNOCC’s are familiar with customer accounts and are
-      knowledgeable, extensively trained (including certified on vendor products)
-      and able to address customer questions. Experienced Network Security
-      Engineers are available to provide additional support and expertise.`,
-    },
-  ]
+  const serviceFeatures = computed<IconWithText[]>(() => {
+    return (overviewThree.value?.meta?.serviceFeatures as IconWithText[]) || []
+  })
 </script>
 
 <template>
@@ -119,14 +78,7 @@
 
       <div class="container">
         <div class="section-lead">
-          <p>
-            first*defense is centered on providing a robust managed security
-            solution while exceeding customer service expectations. An
-            organization's security posture is dynamic and often has unique
-            requirements. Nexum can assist regardless of customer size or
-            vertical leveraging our over two decades of cybersecurity
-            experience.
-          </p>
+          <ContentRenderer :value="overviewThree" />
         </div>
 
         <div class="icon-text-grid">
@@ -145,7 +97,7 @@
               />
             </div>
             <div class="grid-item">
-              <h4>{{ row.title }}</h4>
+              <h4>{{ index + 1 }}. {{ row.title }}</h4>
               <p>{{ row.text }}</p>
             </div>
           </div>
@@ -204,6 +156,7 @@
     text-align: center;
     max-width: 1024px;
     margin: 0 auto 120px;
+    :deep(p),
     p {
       font-size: clamp(18px, 5vw, 22px);
       font-weight: 600;
---
+++ content/services/first-defense/overview-three.md
@@ -0,0 +1,61 @@
+---
+title: 'Overview'
+route: '/services/first-defense'
+anchor: 'firstDefenseOverviewThree'
+icons:
+  - name: 'LogoFirstDefense'
+    url: null
+    link: null
+    alt: 'first*defense logo'
+    theme: 'white'
+
+serviceFeatures:
+  - imgSrc: '/icons/icon-fd-speed.png'
+    imgAlt: 'Speed'
+    title: 'Speed'
+    text: >
+      Rapid collection and processing get alerts to our analysts usually
+      within 60 seconds. Notification of important events to customers
+      occurs within minutes and emergency changes are processed promptly
+      which means faster time to resolution and less risk of a breach.
+
+  - imgSrc: '/icons/icon-fd-data.png'
+    imgAlt: 'Data'
+    title: 'Threat Intelligence'
+    text: >
+      Data from over 100 sources of threat intelligence are ingested in FD
+      solution to identify malicious activity and proactively prevent harm
+      to your organization. Access to consolidated threat intelligence feeds
+      is available to all FD customer.
+
+  - imgSrc: '/icons/icon-fd-capabilities.png'
+    imgAlt: 'Data'
+    title: 'Security Monitoring and Management'
+    text: >
+      Advanced detection and automation capabilities are provided for your
+      systems (whether you have your own SIEM or leverage Nexum’s). The
+      solution is delivered with accountability in mind and customers can
+      elect to retain some or all of the control and activity of the managed
+      platform.
+
+  - imgSrc: '/icons/icon-fd-procedure.png'
+    imgAlt: 'Data'
+    title: 'Flexibility and Transparency'
+    text: >
+      Procedures specific to your organization are customized and transparent
+      access is provided to all workflow notes, even for false positives and
+      non-critical events. Alerts are clear, concise, with a simple outline of
+      cause and a straightforward direction to resolution.
+
+  - imgSrc: '/icons/icon-fd-engineers.png'
+    imgAlt: 'Data'
+    title: 'People'
+    text: >
+      Analysts at our SNOCC’s are familiar with customer accounts and are
+      knowledgeable, extensively trained (including certified on vendor
+      products) and able to address customer questions. Experienced Network
+      Security Engineers are available to provide additional support and
+      expertise.
+---
+
+first\*defense is centered on providing a robust managed security solution while exceeding customer service expectations. An organization's security posture is dynamic and often has unique requirements. Nexum can assist regardless of customer size or vertical leveraging our over two decades of cybersecurity experience.
